fb8910aae34e14df0612e5b4d81fcfed0fa515bd
amirisback <faisalamircs@gmail.com>
2025-02-27 11:46:34 +0700

    update: auto scroll banner

--- app/src/main/java/com/qomunal/opensource/androidresearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/qomunal/opensource/androidresearch/ui/main/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.qomunal.opensource.androidresearch.ui.main
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.qomunal.opensource.androidresearch.common.base.BaseActivity
 import com.qomunal.opensource.androidresearch.databinding.ActivityMainBinding
+import java.util.Timer
+import java.util.TimerTask
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -15,7 +20,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         MainAdapter()
     }
 
-    private val linearLayoutManager : LinearLayoutManager by lazy {
+    private val linearLayoutManager: LinearLayoutManager by lazy {
         LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 
@@ -34,6 +39,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                 adapter = mAdapter
                 layoutManager = linearLayoutManager
             }
+
+            val snapHelper = LinearSnapHelper()
+            snapHelper.attachToRecyclerView(rv)
+
+            val timer = Timer()
+            timer.schedule(object : TimerTask() {
+                override fun run() {
+                    Handler(mainLooper).post {
+                        if (linearLayoutManager.findLastCompletelyVisibleItemPosition() < mAdapter.itemCount - 1) {
+                            linearLayoutManager.smoothScrollToPosition(rv, RecyclerView.State(), linearLayoutManager.findLastCompletelyVisibleItemPosition() + 1)
+                        } else {
+                            linearLayoutManager.smoothScrollToPosition(rv, RecyclerView.State(), 0)
+                        }
+                    }
+                }
+            }, 0, 4000)
+
         }
     }
 
